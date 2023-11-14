7faf1f8cf42345e3adde882894a24cd07bb705f0
y.kareba <y.kareba@g.nsu.ru>
2023-11-15 01:04:24 +0700

    Made merge

--- 38.py
+++ 38.py
@@ -8,7 +8,6 @@ class Solution:
         for i in range(1, n+1):
             for j in range(0, m+1):
                 if p[i - 1] == '*':
-                    if j == 1: print(a[i-2][j])
                     a[i][j] = a[i-2][j] or ( (j > 0) and (s[j-1] == p[i-2] or p[i-2] == '.') and a[i][j-1])
                 else:
                     a[i][j] = (j>0) and (s[j-1] == p[i-1] or p[i-1] == '.') and a[i-1][j-1]
--- 40.py
+++ 40.py
@@ -14,6 +14,30 @@ class Node:
     def __str__(self):
         return f'p: {self.prioriety}, v: {self.val}, rank: {self.rank}, s: {self.sum} '
 
+def merge(node1, node2):
+    if not node1: return node2
+    if not node2: return node1
+
+    if node1.prioriety < node2.prioriety:
+        node1.right = merge(node1.right, node2)
+        update(node1)
+        return node1
+    else:
+        node2.left = merge(node1, node2.left)
+        update(node2)
+        return node2
+
+def make(a):
+    n = len(a)
+    priorities = [i for i in range(n)]
+    shuffle(priorities)
+    nodes = [Node(priorities[i], a[i], 1, a[i]) for i in range(n)]
+    root = nodes[0]
+    for i in range(1, n):
+        root = merge(root, nodes[i])
+        print(serialize(root))
+    return root
+'''
 def makeTreap(a) -> Node:
     n = len(a)
     priorities = [i for i in range(n)]
@@ -26,6 +50,13 @@ def makeTreap(a) -> Node:
         if prev.prioriety < curr.prioriety:
             prev.right = curr
             curr.par = prev
+            prev.sum += curr.sum
+            prev.rank += curr.rank
+            prev = prev.par
+            while prev:
+                prev.sum += curr.sum
+                prev.rank += curr.rank
+                prev = prev.par
         else:
             while(prev.par and prev.par.prioriety > curr.prioriety):
                 prev = prev.par
@@ -35,15 +66,18 @@ def makeTreap(a) -> Node:
             else:
                 prev.par.right = curr
                 prev.par, curr.par = curr, prev.par
+                curr.par.sum += curr.sum
+                curr.par.rank += 1
+            curr.rank += prev.rank
+            curr.sum += prev.sum
             curr.left = prev
     return root
-
+'''
 def printTreapAsArr(root):
     if not root : return
     printTreapAsArr(root.left)
     print(root.val, end = ' ')
     printTreapAsArr(root.right)
-
 
 def update(root):
     rank_left = 0 if not root.left else root.left.rank
@@ -51,13 +85,15 @@ def update(root):
     rank_right = 0 if not root.right else root.right.rank
     sum_right = 0 if not root.right else root.right.sum
     root.rank = 1 + rank_left + rank_right
+    root.sum = sum_left + sum_right + root.val
+
 def serialize(root):
     def dfs(root, arr):
         if not root:
             arr.append('x')
             return
         arr.append('(')
-        arr.append(f'[{root.val}, {root.prioriety}]')
+        arr.append(f'[{root.val}, {root.rank}, {root.sum}]')
         dfs(root.left, arr)
         dfs(root.right, arr)
         arr.append(')')
@@ -65,6 +101,7 @@ def serialize(root):
     dfs(root, arr)
     return(' '.join(arr))
 
-l = [1, 2, 3, 4, 5]
-root = makeTreap(l)
+l = [1,2,3,4,5]
+root = make(l)
 print(serialize(root))
+printTreapAsArr(root)
